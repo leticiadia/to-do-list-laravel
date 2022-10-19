e2f3cbd49e83f60ce6255c28a8b31b93e6e6dd4b
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-19 09:44:26 -0300

    feat: add save and cancel button in modal

--- resources/views/templates/form.blade.php
+++ resources/views/templates/form.blade.php
@@ -5,24 +5,24 @@
             <h3>Create task</h3>
         </div>
 
-        <form action="{{ route('store') }}" method="post">
+        <form action="{{ route('store') }}" method="post" class="form">
             <div class="input-name">
-                <label for="name">Task Name</label>
+                <label for="name">Task Name:</label>
                 <input type="text" id="name" name="name" placeholder="Name" />
             </div>
 
             <div class="input-label">
-                <label for="label">Days of Week</label>
+                <label for="label">Days of Week:</label>
                 <input type="text" id="label" name="label" placeholder="Days of Week" />
             </div>
 
             <div class="buttons">
                 <div class="btn-save">
-                    <a href="#">Save</a>
+                    <button type="submit">Save</button>
                 </div>
 
                 <div class="btn-cancel">
-                    <a href="#">Cancel</a>
+                    <button type="submit">Cancel</button>
                 </div>
             </div>
         </form>
