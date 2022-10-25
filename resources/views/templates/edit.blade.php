392d17e9093409ebae768156a98cf0abef4ab302
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-24 21:35:45 -0300

    feat: template edit created

---
+++ resources/views/templates/edit.blade.php
@@ -0,0 +1,32 @@
+<div class="modal-content-edit">
+    <div class="modal-body">
+        <x-eos-close width="24" height="24" onclick="ModalEdit.close()" class="modal-close" />
+        <div class="modal-header">
+            <h3>Edit task</h3>
+        </div>
+
+        <form action="/task/{{ $task->id }}" method="post" class="form">
+            @csrf
+            @method('PUT')
+            <div class="input-name">
+                <label for="name">Task Name:</label>
+                <input type="text" id="name" name="name" placeholder="Name" value="{{ $task->name }}" />
+            </div>
+
+            <div class="input-label">
+                <label for="label">Days of Week:</label>
+                <input type="text" id="label" name="label" placeholder="Days of Week" value="{{ $task->label }}" />
+            </div>
+
+            <div class="buttons">
+                <div class="btn-save">
+                    <button type="submit">Save</button>
+                </div>
+
+                <div class="btn-cancel">
+                    <button type="submit">Cancel</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
