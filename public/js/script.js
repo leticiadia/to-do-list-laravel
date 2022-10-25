c4813089bb6065f002152c82236b4031beb00f2a
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-24 21:31:07 -0300

    feat: open() and close() method created

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,32 @@
-const modal = document.querySelector(".btn-new");
-const modalActive = document.querySelector(".modal-content");
-const modalClose = document.querySelector(".modal-close");
+// const modal = document.querySelector(".btn-new");
+// const modalActive = document.querySelector(".modal-content");
+// const modalClose = document.querySelector(".modal-close");
 
-modal.addEventListener("click", () => {
-    modalActive.classList.add("modal-active");
-});
+// modal.addEventListener("click", () => {
+//     modalActive.classList.add("modal-active");
+// });
 
-modalClose.addEventListener("click", () => {
-    modalActive.classList.remove("modal-active");
-});
+// modalClose.addEventListener("click", () => {
+//     modalActive.classList.remove("modal-active");
+// });
+
+
+const Modal = {
+    open() {
+       document.querySelector('.modal-content').classList.add('modal-active'); 
+    },
+
+    close() {
+        document.querySelector('.modal-content').classList.remove('modal-active');
+    }
+}
+
+const ModalEdit = {
+    open() {
+       document.querySelector('.modal-content-edit').classList.add('modal-active'); 
+    },
+
+    close() {
+        document.querySelector('.modal-content-edit').classList.remove('modal-active');
+    }
+}
