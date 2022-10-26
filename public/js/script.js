5b8074a5a1407c7e954084032099df7e84e648d5
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-26 13:00:43 -0300

    feat: get value of name and label inputs

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,30 @@
-// const modal = document.querySelector(".btn-new");
-// const modalActive = document.querySelector(".modal-content");
-// const modalClose = document.querySelector(".modal-close");
-
-// modal.addEventListener("click", () => {
-//     modalActive.classList.add("modal-active");
-// });
-
-// modalClose.addEventListener("click", () => {
-//     modalActive.classList.remove("modal-active");
-// });
-
-
 const Modal = {
     open() {
-       document.querySelector('.modal-content').classList.add('modal-active'); 
+        document.querySelector(".modal-content").classList.add("modal-active");
     },
 
     close() {
-        document.querySelector('.modal-content').classList.remove('modal-active');
-    }
-}
+        document
+            .querySelector(".modal-content")
+            .classList.remove("modal-active");
+    },
+};
 
 const ModalEdit = {
-    open() {
-       document.querySelector('.modal-content-edit').classList.add('modal-active'); 
+    open(name, label) {
+        const modal = document.querySelector(".modal-content-edit");
+
+        document
+            .querySelector(".modal-content-edit")
+            .classList.add("modal-active");
+
+        modalBodyInputName = modal.querySelector(".name-input").value = name;
+        modalBodyInputLabel = modal.querySelector(".label-input").value = label;
     },
 
     close() {
-        document.querySelector('.modal-content-edit').classList.remove('modal-active');
-    }
-}
+        document
+            .querySelector(".modal-content-edit")
+            .classList.remove("modal-active");
+    },
+};
