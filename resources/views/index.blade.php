e39d4e0969a7d5cba0a21e07907255f49a27a340
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-27 21:32:28 -0300

    feat: add files app.css and style.css

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/styles/style.css">
+    @vite('resources/css/app.css')
+    @vite('resources/css/style.css')
+
     <title>ToDoList</title>
 </head>
 
