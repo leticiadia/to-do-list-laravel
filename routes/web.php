f40278d5f0bc0b79e6e1df92b9da5fa8180bccdf
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-27 21:40:43 -0300

    feat: added routes create-task and delete

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TasksController::class, 'index'])->name('tasks');
 
+Route::get('/create-task', [TasksController::class, 'create'])->name('tasks.create');
 Route::post('/task', [TasksController::class, 'store'])->name('store');
 
 Route::get('/task/{id}/edit', [TasksController::class, 'edit'])->name('tasks.edit');
 Route::put('/task/{id}', [TasksController::class, 'update'])->name('tasks.update');
+
+Route::delete('/task/{id}', [TasksController::class, 'destroy'])->name('tasks.destroy');
