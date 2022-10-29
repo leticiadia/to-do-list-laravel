964d56792467e3dee81dfa1191ed3fd2527c005c
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-29 12:23:21 -0300

    feat: added Tailwind styles for formatting inputs and labels

--- resources/views/templates/edit-task.blade.php
+++ resources/views/templates/edit-task.blade.php
@@ -1,18 +1,18 @@
 @extends('index')
 
 @section('content')
-<div class="form bg-white shadow-lg h-96 w-2/4 p-20 rounded mx-auto ">
-    <form action="/task/{{ $task->id }}" method="post" class="w-full max-w-lg mx-auto flex flex-col justify-center">
+<div class="form bg-white shadow-md h-2/3 w-2/4 p-20 flex flex-col justify-center items-center rounded mx-auto">
+    <form action="/task/{{ $task->id }}" method="post" class="w-full mx-auto flex flex-col justify-center">
         @method('PUT')
         @csrf
         <div class="input-name">
-            <label for="name" class="block uppercase tracking-wide text-violet-500 text-base font-bold mb-2">Task Name:</label>
-            <input type="text" id="name" name="name" placeholder="Name" class="text-sm appearance-none block w-full bg-gray-100 text-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-violet-500" value="{{ $task->name }}" />
+            <label for="name" class="block uppercase tracking-wide text-violet-400 text-base font-semibold font-sans mb-2">Task Name:</label>
+            <input type="text" id="name" name="name" placeholder="Name" class="text-sm font-sans appearance-none block w-full bg-gray-200 text-gray-500 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:outline-violet-500" value="{{ $task->name }}" />
         </div>
 
         <div class="input-label">
-            <label for="label" class="block uppercase tracking-wide text-violet-500 text-base font-bold mb-2">Days of Week:</label>
-            <input type="text" id="label" name="label" placeholder="Days of Week" class="text-sm appearance-none block w-full bg-gray-100 text-gray-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-violet-500" value="{{ $task->label }}" />
+            <label for="label" class="block uppercase tracking-wide text-violet-400 text-base font-semibold font-sans mb-2">Days of Week:</label>
+            <input type="text" id="label" name="label" placeholder="Days of Week" class="text-sm font-sans appearance-none block w-full bg-gray-200 text-gray-500 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:outline-violet-500" value="{{ $task->label }}" />
         </div>
 
         <div class="flex items-center justify-end mt-2">
