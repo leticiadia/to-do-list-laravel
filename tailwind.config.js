a9ff9e423d90c3e918d7c4e15125bb20f8107ffd
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-29 12:07:50 -0300

    feat: add property fontFamily to import and used Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
