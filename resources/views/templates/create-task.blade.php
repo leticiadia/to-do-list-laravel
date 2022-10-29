eb2e8b066e5dde06794596219b8f87ad3e20a8fa
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-29 12:22:48 -0300

    feat: added Tailwind styles for formatting inputs and labels and added route to redirect to homepage

--- resources/views/templates/create-task.blade.php
+++ resources/views/templates/create-task.blade.php
@@ -1,17 +1,17 @@
 @extends('index')
 
 @section('content')
-<div class="form bg-white shadow-lg h-96 w-2/4 p-16 rounded mx-auto ">
-    <form action="{{ route('store') }}" method="post" class="w-full max-w-lg flex flex-col justify-center">
+<div class="form bg-white shadow-md h-2/3 w-2/4 p-20 flex flex-col justify-center items-center rounded mx-auto">
+    <form action="{{ route('store') }}" method="post" class="w-full mx-auto flex flex-col justify-center">
         @csrf
         <div class="input-name">
-            <label for="name" class="block uppercase tracking-wide text-violet-500 text-base font-bold mb-2">Task Name:</label>
-            <input type="text" id="name" name="name" placeholder="Name" class="text-sm appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+            <label for="name" class="block uppercase tracking-wide text-violet-400 text-base font-semibold font-sans mb-2">Task Name:</label>
+            <input type="text" id="name" name="name" placeholder="Name" class="text-sm font-sans appearance-none block w-full bg-gray-200 text-gray-500 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:outline-violet-500" />
         </div>
 
         <div class="input-label">
-            <label for="label" class="block uppercase tracking-wide text-violet-500 text-base font-bold mb-2">Days of Week:</label>
-            <input type="text" id="label" name="label" placeholder="Days of Week" class="text-sm appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" />
+            <label for="label" class="block uppercase tracking-wide text-violet-400 text-base font-semibold font-sans mb-2">Days of Week:</label>
+            <input type="text" id="label" name="label" placeholder="Days of Week" class="text-sm font-sans appearance-none block w-full bg-gray-200 text-gray-500 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:bg-white focus:outline-violet-500" />
         </div>
 
         <div class="flex items-center justify-end">
@@ -20,7 +20,7 @@
             </div>
 
             <div class="btn-cancel">
-                <button type="submit" class="bg-red-400 hover:bg-red-500 text-white text-sm font-bold py-3 px-10 rounded">Cancel</button>
+                <a href="{{ route('tasks') }}" class="no-underline bg-red-400 hover:bg-red-500 text-white text-sm font-bold py-3 px-10 rounded">Cancel</a>
             </div>
         </div>
     </form>
