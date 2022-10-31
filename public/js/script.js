10c6ee0c33a26690064ee94b4cf2c0cb9329258f
Leticia Dias <contatoleticiadia@gmail.com>
2022-10-31 16:19:45 -0300

    refactor: removed ModalEdit object

--- public/js/script.js
+++ public/js/script.js
@@ -1,30 +1,36 @@
+// const Modal = {
+//     open() {
+//         document.querySelector(".modal-content").classList.add("modal-active");
+//     },
+
+//     close() {
+//         document
+//             .querySelector(".modal-content")
+//             .classList.remove("modal-active");
+//     },
+// };
+
+// const ModalEdit = {
+//     open(name, label) {
+//         const modal = document.querySelector(".modal-content-edit");
+
+//         document
+//             .querySelector(".modal-content-edit")
+//             .classList.add("modal-active");
+
+//         modalBodyInputName = modal.querySelector(".name-input").value = name;
+//         modalBodyInputLabel = modal.querySelector(".label-input").value = label;
+//     },
+
+//     close() {
+//         document
+//             .querySelector(".modal-content-edit")
+//             .classList.remove("modal-active");
+//     },
+// };
+
 const Modal = {
     open() {
-        document.querySelector(".modal-content").classList.add("modal-active");
-    },
-
-    close() {
-        document
-            .querySelector(".modal-content")
-            .classList.remove("modal-active");
-    },
-};
-
-const ModalEdit = {
-    open(name, label) {
-        const modal = document.querySelector(".modal-content-edit");
-
-        document
-            .querySelector(".modal-content-edit")
-            .classList.add("modal-active");
-
-        modalBodyInputName = modal.querySelector(".name-input").value = name;
-        modalBodyInputLabel = modal.querySelector(".label-input").value = label;
-    },
-
-    close() {
-        document
-            .querySelector(".modal-content-edit")
-            .classList.remove("modal-active");
-    },
-};
+        document.querySelector('.modal').classList.add('modal-active');
+    }
+}
